5b822b2f00ab5826957e34de6c32146d6996701b
Sreedharun <sreedharuncr7@gmail.com>
2026-08-26 22:33:22 +0530

    Time: 3 ms (25.52%), Space: 18.1 MB (5.46%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int t=0,fresh=0;
+        int t=0,fres=0;
         queue<vector<int>> q;
-        int row=grid.size();
-        int col=grid[0].size();
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        int row=grid.size(),col=grid[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
                 }else if(grid[i][j]==1){
-                    fresh++;
+                    fres++;
                 }
             }
         }
-        vector<vector<int>> dir={{0,-1},{-1,0},{0,1},{1,0}};
-        while(!q.empty() && fresh>0){
+        vector<vector<int>> dir={{0,1},{1,0},{-1,0},{0,-1}};
+        while(!q.empty() && fres>0){
             t++;
-            int size=q.size();
-            for(int m=0;m<size;m++){
-                vector<int> v= q.front();
-                for(int k=0;k<4;k++){
-                    int rr=v[0]+dir[k][0];
-                    int rc=v[1]+dir[k][1];
+            int s=q.size();
+            for(int m=0;m<s;m++){
+                vector<int> v=q.front();
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int rr=v[0]+dir[i][0];
+                    int rc=v[1]+dir[i][1];
                     if(rr>=0 && rc>=0 && rr<row && rc<col && grid[rr][rc]==1){
                         grid[rr][rc]=2;
                         q.push({rr,rc});
-                        fresh--;
+                        fres--;
                     }
                 }
-                q.pop();
             }
         }
-        return fresh==0?t:-1;
+        return fres==0?t:-1;
     }
 };
