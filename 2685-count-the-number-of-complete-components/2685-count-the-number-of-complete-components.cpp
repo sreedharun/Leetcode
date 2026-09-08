60f6b1ff749ff1994b69f5220f9bc981ff156c1d
Sreedharun <sreedharuncr7@gmail.com>
2026-09-08 11:56:14 +0530

    Time: 46 ms (50.75%), Space: 132 MB (46.21%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,61 +1,43 @@
 class Solution {
 public:
-    int c=-1;    
-    void dfs(int s,vector<bool>& vis,vector<vector<int>>& adj,vector<vector<int>>& com){
+    int vec,edge;
+    vector<int> dfs(int s,vector<vector<int>>& adj,vector<bool>& vis){
         vis[s]=true;
-        com[c].push_back(s);
-        for(int i = 0;i < adj[s].size() ;i++){
+        vec++;
+        edge+=adj[s].size();
+        for(int i=0;i<adj[s].size();i++){
             if(!vis[adj[s][i]]){
-                dfs(adj[s][i],vis,adj,com);
+                dfs(adj[s][i],adj,vis);
             }
         }
+        return {vec,edge};
     }
-    int count(int n,vector<bool>& vis,vector<vector<int>>& adj,vector<vector<int>>& com){
+    bool check(int n1,int n2){
+        int val=(n1*(n1-1))/2;
+        return (n2/2)==val;
+    }
+    int count(int n,vector<vector<int>>& adj,vector<bool>& vis){
+        int c=0;
         for(int i=0;i<n;i++){
             if(vis[i]==false){
-                c++;
-                dfs(i,vis,adj,com);
+                vec=0;
+                edge=0;
+                vector<int> val=dfs(i,adj,vis);
+                if(check(val[0],val[1])){
+                    c++;
+                }
             }
         }
         return c;
     }
-    bool las(int p1,int p2,vector<vector<int>>& edges){
-        for(int i=0;i<edges.size();i++){
-            if(p1==edges[i][0] && p2==edges[i][1] || 
-            p2==edges[i][0] && p1==edges[i][1]){
-                return true;
-            }
-        }
-        return false;
-    }
-    bool find(vector<int> com,vector<vector<int>>& edges){
-        int n=com.size();
-        if(n==1){return true;}
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(!las(com[i],com[j],edges)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
         vector<bool> vis(n,false);
-        vector<vector<int>> adj(n);
-        vector<vector<int>> com(n);
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
+        vector<vector<int>> adj(n,vector<int>(0));
+        for(auto& e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
-        count(n,vis,adj,com);
-        int res=0;
-        for(int i=0;i<com.size();i++){
-            if(com[i].empty()){continue;}
-            if(find(com[i],edges)){
-                res++;
-            }
-        }
-        return res;
+        int ans=count(n,adj,vis);
+        return ans;
     }
 };
