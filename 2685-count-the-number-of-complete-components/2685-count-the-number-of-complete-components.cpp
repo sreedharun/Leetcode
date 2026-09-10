7ff3bcf6fcbdc03337d9026d5a3e46d9a6ccb53c
Sreedharun <sreedharuncr7@gmail.com>
2026-09-10 10:00:28 +0530

    Time: 75 ms (19.8%), Space: 139.2 MB (18.5%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    int vec,edge;
-    vector<int> dfs(int s,vector<vector<int>>& adj,vector<bool>& vis){
-        vis[s]=true;
-        vec++;
-        edge+=adj[s].size();
-        for(int i=0;i<adj[s].size();i++){
-            if(!vis[adj[s][i]]){
-                dfs(adj[s][i],adj,vis);
-            }
-        }
-        return {vec,edge};
-    }
-    bool check(int n1,int n2){
-        int val=(n1*(n1-1))/2;
-        return (n2/2)==val;
-    }
-    int count(int n,vector<vector<int>>& adj,vector<bool>& vis){
-        int c=0;
-        for(int i=0;i<n;i++){
-            if(vis[i]==false){
-                vec=0;
-                edge=0;
-                vector<int> val=dfs(i,adj,vis);
-                if(check(val[0],val[1])){
-                    c++;
+    int ver,ed;
+    vector<int> dfs(int s,vector<vector<int>>& aj,vector<bool>& vis){
+            vis[s]=true;
+            ver++;
+            ed+=aj[s].size();
+            for(int i=0;i<aj[s].size();i++){
+                if(!vis[aj[s][i]]){
+                    dfs(aj[s][i],aj,vis);
                 }
             }
-        }
-        return c;
+            return {ver,ed};
+    }
+    bool check(int v1,int v2){
+        int t=(v1*(v1-1))/2;
+        return t==v2/2;
+    }
+    int count(int n,vector<vector<int>>& aj,vector<bool> vis){
+            int c=0;
+            for(int i=0;i<n;i++){
+                if(vis[i]==false){
+                    ver=0;
+                    ed=0;
+                    vector<int> val=dfs(i,aj,vis);
+                    if(check(val[0],val[1])){
+                        c++;
+                    }
+                }
+            }
+            return c;
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> aj(n);
         vector<bool> vis(n,false);
-        vector<vector<int>> adj(n,vector<int>(0));
-        for(auto& e:edges){
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+        for(auto v:edges){
+            aj[v[0]].push_back(v[1]);
+            aj[v[1]].push_back(v[0]);
         }
-        int ans=count(n,adj,vis);
-        return ans;
+        int c=count(n,aj,vis);
+        return c;
     }
 };
